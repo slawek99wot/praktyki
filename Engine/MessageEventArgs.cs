7a7cfa6ac9762e820a10dd4bc827b2df47aca185
Sławek <Sawek>
2017-03-24 15:16:26 +0100

    Game

---
+++ Engine/MessageEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine
+{
+    internal class MessageEventArgs
+    {
+        private bool addExtraNewLine;
+        private string message;
+
+        public MessageEventArgs(string message, bool addExtraNewLine)
+        {
+            this.message = message;
+            this.addExtraNewLine = addExtraNewLine;
+        }
+    }
+}
---
+++ SuperGame/SuperGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Engine;
+namespace SuperGame
+{
+    public partial class SuperGame : Form
+    {
+        private Player _player;
+
+        public SuperGame()
+        {
+           InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExpPoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExp.Text = _player.ExpPoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
